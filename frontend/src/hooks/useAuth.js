f0c88fd8f99a95da99211bd2007d6a888d111883
Rauan <asetovrauan0@gmail.com>
2025-11-07 14:30:17 +0500

    Hook for Auth

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,13 @@
+/**
+ * Custom hook for easily accessing the AuthContext.
+ */
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
